3a27b5310aafec700ebc8b9a232c6b89165f7fa8
wiggoen <wiggoen@gmail.com>
2019-04-03 15:52:13 +0200

    added sorting scripts

---
+++ Scripts/sorting/AQ4S.sh
@@ -0,0 +1,99 @@
+# Save all command line arguments even if shift is used
+argument_counter="$#";
+arguments="$@";
+element="$1";
+calibration="$2";
+sorting="$3";
+the_date=$(date +%F)
+
+function usage {
+	echo "Usage of $0:"
+	echo "Provide three command line arguments; 'element calibration sorter'"
+	echo "e.g. 'Sm online TB' or 'Ni online TB' to calibrate the respective data."
+	# Sm is used for Samarium. If you have other elements, it can be extended quite easily.
+	echo "Element choice: 'Sm' or 'Ni'."
+	echo "Calibration choice: 'online', 'zero' or 'user'."
+	echo "Sorting choice: 'Q4' (AQ4Sort) or 'TB' (Treebuilder)."
+	echo "Number of arguments: $argument_counter arguments."
+	echo "Argument list: $arguments"
+}
+
+if [ $# == 0 ] || [ $# != 3 ]; then
+	usage;
+	exit;
+else
+	# Read first command line argument
+    case "$1" in
+        Sm)
+            run_number=(8 9 10 11 12 16 17 18 19 20 25 26 29 30 31 35 37 39 40 41 42 43 44 45 51 52 53 57 60);
+            ;;  # ;; indicates end of case
+        Ni)
+            run_number=(47 50 52 53 54 55 56 57 58 59); #(52 53 54);
+            ;;
+        *)
+            echo "$0: invalid option \"$1\""; 
+            usage;
+            exit ;;
+    esac
+	# Read second command line argument
+    case "$2" in
+        online)
+			calibrationfile="../../../Miniball/MiniballCoulexSort/config/IS558-online.cal"
+			;;
+		user)
+			calibrationfile="../../../Miniball/MiniballCoulexSort/config/IS558-user.cal"
+			;;
+		zero)
+			calibrationfile="../../../Miniball/MiniballCoulexSort/config/IS558-zero.cal"
+			;;
+        *)
+            echo "$0: invalid option \"$2\""; 
+            usage;
+            exit ;;
+    esac
+	# Read third command line argument
+	case "$3" in
+        Q4)
+			sort_choice="AQ4Sort"
+			flag="";
+			;;
+		TB)
+			sort_choice="TreeBuilder";
+			flag="-cdpad"
+			;;
+        *)
+            echo "$0: invalid option \"$3\""; 
+            usage;
+            exit ;;
+    esac
+fi
+
+filearray=()
+
+for number in ${run_number[*]};
+    do
+		if [ $number -lt 10 ]; then
+				zeros="00";
+			else
+				zeros="0";
+		fi
+    	if [ $element == "Sm" ]; then
+    		directory="../../Raw_data/Sm"
+        	rootfile="$directory/140Sm_208Pb_pos6_${zeros}${number}_OnBeam.root"
+        #elif [ $element == "Ni" ]; then
+        #	directory="../../Raw_data/Ni"
+        #	rootfile="$directory/144Ba_58Ni_pos5_${zeros}${number}_OnBeam.root"
+		fi
+    	for entry in $directory/*;
+			do
+				if [ "$entry" == "$rootfile" ]; then
+					filearray+=("$entry")
+				fi
+			done
+    done 
+
+filearray=${filearray[@]}
+echo "--- $sort_choice ---"
+command="$sort_choice -i $filearray -o ${element}_${calibration}-${sort_choice}-${the_date}.root -c $calibrationfile $flag -vl"
+# Call sorter to calibrate data
+$command
